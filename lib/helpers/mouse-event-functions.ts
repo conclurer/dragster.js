99467f1d6bccb60c34ad753f7e25acff4c1059e7
marvinscharle <marvin.scharle@conclurer.com>
2016-07-13 19:47:40 +0200

    Updated mouse-event-functions

--- lib/helpers/mouse-event-functions.ts
+++ lib/helpers/mouse-event-functions.ts
@@ -1,16 +1,19 @@
-import {IMouseEventMapping, mouseEventName} from "../interfaces/dragster-results";
-export function whichMouseButton(event: (MouseEvent|TouchEvent)): number {
+import {IMouseEventMapping, mouseEventName} from '../interfaces/dragster-results';
+
+export function whichMouseButton(e: MouseEvent | TouchEvent): number {
     if (event instanceof TouchEvent) {
-        let e = <TouchEvent>event;
-        return e.touches.length;
+        return (<TouchEvent>e).touches.length;
     }
     else {
-        let e = <MouseEvent>event;
+        e = <MouseEvent>e;
 
         if (e.which !== void 0 && e.which !== 0) return e.which; // see https://github.com/bevacqua/dragula/issues/261
         if (e.buttons !== void 0) return e.buttons;
-        let button = e.button;
+        let button: number = e.button;
         if (button !== void 0) { // see https://github.com/jquery/jquery/blob/99e8ff1baa7ae341e94bb89c3e84570c7c3ad9ea/src/event.js#L573-L575
+
+            // Disable tslint for bitwise operators
+            // tslint:disable-next-line
             return button & 1 ? 1 : button & 2 ? 3 : (button & 4 ? 2 : 0);
         }
 
@@ -19,17 +22,17 @@ export function whichMouseButton(event: (MouseEvent|TouchEvent)): number {
 }
 
 export function getEventNames(defaultEvent: mouseEventName): string[] {
-    let touch = <IMouseEventMapping>{
+    let touch: IMouseEventMapping = {
         mouseup: 'touchend',
         mousedown: 'touchstart',
         mousemove: 'touchmove'
     };
-    let pointers = <IMouseEventMapping>{
+    let pointers: IMouseEventMapping = {
         mouseup: 'pointerup',
         mousedown: 'pointerdown',
         mousemove: 'pointermove'
     };
-    let microsoft = <IMouseEventMapping>{
+    let microsoft: IMouseEventMapping = {
         mouseup: 'MSPointerUp',
         mousedown: 'MSPointerDown',
         mousemove: 'MSPointerMove'
